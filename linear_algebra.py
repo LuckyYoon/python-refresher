fb34484937f61363c04f7ca591ed8ff00070dbda
LuckyYoon <justinyoon1234@gmail.com>
2024-07-12 07:46:31 -0400

    Linear Algebra Fix

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,49 +1,49 @@
 import numpy as np
 
 # Problem 1
-print("#1")
+print("#1", end=" ")
 
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 
-print("sum: ", a + b)
-print("difference: ", a - b)
+print("sum: \n", a + b)
+print("difference: \n", a - b)
 
 # Problem 2
-print("\n", "#2")
+print("\n", "#2", end=" ")
 
 a = np.array([[1, 2], [3, 4]])
 b = np.array([[5, 6], [7, 8]])
 
-print("sum: ", a + b)
-print("difference: ", a - b)
+print("sum: \n", a + b)
+print("difference: \n", a - b)
 
 # Problem 3
-print("\n", "#3")
+print("\n", "#3", end=" ")
 
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 
-print("dot: ", np.dot(a, b))
+print("dot: \n", np.dot(a, b))
 
 # Problem 4
-print("\n", "#4")
+print("\n", "#4", end=" ")
 
 a = np.array([[1, 2, 3], [4, 5, 6]])
 b = np.array([[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]])
 
-print("product: ", np.dot(a, b))
+print("product: \n", np.dot(a, b))
 
 # Problem 5
-print("\n", "#5")
+print("\n", "#5", end=" ")
 
 a = np.array([1, 1, 2])
 
-print("magnitude: ", np.linalg.norm(a))
+print("magnitude: \n", np.linalg.norm(a))
 
 # Problem 6
-print("\n", "#6")
+print("\n", "#6", end=" ")
 
 a = np.array([[1, 2], [3, 4]])
 
-print("transpose: ", a.T)
+print("transpose: \n", a.T)
