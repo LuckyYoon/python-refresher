1bf98fcdad6d4f0997282d6e1ee28a43874e314d
LuckyYoon <justinyoon1234@gmail.com>
2024-07-12 14:16:56 -0400

    Physics Refresher Save

---
+++ physics.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+g = 9.81
+
+
+# Problem 1
+def calculate_buoyancy(volume, density_fluid):
+    return density_fluid * volume * g
+
+
+# Problem 2
+def will_it_float(volume, mass):
+    if mass / volume < 1000:
+        return True
+    else:
+        return False
+
+
+# Problem 3
+def calculate_pressure(depth):
+    return depth * g * 1000
+
+
+# Problem 4
+def calculate_acceleration(force, mass):
+    return force / mass
+
+
+# Problem 5
+def calculate_angular_acceleration(torque, I):
+    return torque / I
+
+
+# Problem 6
+def calculate_torque(F_magnitude, F_direction, radius):
+    F_direction = F_direction * (np.pi / 180)
+    return radius * F_magnitude * np.sin(F_direction)
+
+
+# Problem 7
+def calculate_moment_of_inertia(mass, radius):
+    return mass * radius**2
+
+
+# Problem 8
+def calculate_auv_acceleration(F_magnitude, F_angle, mass, volume, thruster_distance):
+    return (
+        np.sqrt(
+            (np.sin(F_angle) * F_magnitude + volume * g * 1000) ** 2
+            + (np.cos(F_angle) * F_magnitude) ** 2
+        )
+        / mass
+    )
+
+
+# MAYBE??? SEPERATED X AND Y COMPONENTS
+# np.sqrt((np.sin(F_angle) * F_magnitude + volume * g * 1000) ** 2 + (np.cos(F_angle) * F_magnitude) ** 2) / mass
+# OR
+# np.sqrt((F_magnitude * thruster_distance * np.sin(F_angle)) ** 2 + (volume * g * 1000) ** 2) / mass
+
+
+def calculate_auv_angular_acceleration(
+    F_magnitude, F_angle, inertia, thruster_distance
+):
+    return (F_magnitude * thruster_distance * np.sin(F_angle)) / inertia
+
+
+# Problem 9
+def calculate_auv2_acceleration(T, alpha, theta, mass):
+    xComp = 0
+    yComp = 0
+
+    for i in T:
+        xComp = xComp + np.cos(alpha) * i
+        yComp = yComp + np.sin(alpha) * i
+
+    return np.sqrt(xComp**2 + yComp**2) / mass
+
+
+def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia):
+    distance = np.sqrt(L**2 + l**2)
+    torque = distance * -T[0] * np.sin(alpha)
+    torque = torque + distance * T[1] * np.sin(alpha)
+    torque = torque + distance * -T[2] * np.sin(alpha)
+    torque = torque + distance * T[3] * np.sin(alpha)
+    return torque / inertia
+
+
+# Problem 10
+def simulate_auv2_motion(T, alpha, L, l, mass, inertia, dt, t_final, x0, y0, theta0):
+    t = np.array[t_final + 1]
+    X = np.array[t_final + 1]
+    y = np.array[t_final + 1]
+    theta = np.array[t_final + 1]
+    v = np.array[t_final + 1]
+    omega = np.array[t_final + 1]
+    a = np.array[t_final + 1]
+
+    for i in range(0, t_final + 1):
+        t[i] = i
+        a[t[i]] = calculate_auv2_acceleration(T, alpha, theta)
